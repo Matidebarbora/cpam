0031e66b084091cd084b4647111b926be66de6c6
Matidebarbora <matias.debarbora@hotmail.com>
2026-05-04 17:35:32 -0400

    menú cíclico y manejo de errores de inputs

--- main.py
+++ main.py
@@ -1,41 +1,55 @@
 from calculos import replanteo_y_amojonamiento
 
-opciones = [1,2,3]
-
-print ("\n" + "="*49)
-print ("====== PROGRAMA PARA CÁLCULO DE HONORARIOS ======")
-print ("="*49)
-print ("\nMENU")
-print ("\nACTAS DE AMOJONAMIENTOS")
-print ("1. Replanteo y amojonamiento")
-
-opt = None
+opciones = [1,2,3,0]
 
 while True:
-    try:
-        entrada = int(input("\nElija una opción: "))
-        if entrada in opciones:
-            opt = entrada
-            break
-        else:
-            print ("Opción inválida, vuelva a intentar")
-    except ValueError:
-        print ("Opción inválida, vuelva a intentar")
 
-if (opt == 1):
+    print ("\n" + "="*49)
+    print ("====== PROGRAMA PARA CÁLCULO DE HONORARIOS ======")
+    print ("="*49)
+    print ("\nMENU")
+    print ("-"*49)
+    print ("ACTAS DE AMOJONAMIENTOS")
+    print ("1. Replanteo y amojonamiento")
+    print ("-"*49)
+    print ("0. Salir")
+
+    opt = None
+
     while True:
         try:
-            nro_parcelas = int(input("Ingrese cantidad de parcelas: "))
-            recargo = int(input("Ingrese % de recargo de honorarios: "))
-            honorarios, honorarios_extra, sellado_cpam = replanteo_y_amojonamiento(nro_parcelas, recargo)
-            #print (type(honorarios))
-            print (f"\nHonorarios: ${honorarios}")
-            if recargo != 0:
-                #print (type(honorarios_extra))
-                print (f"Honorarios + {recargo}%: ${honorarios_extra}")
+            entrada = int(input("\nElija una opción: "))
+            if entrada in opciones:
+                opt = entrada
                 break
             else:
-                break
+                print ("Opción inválida, vuelva a intentar")
         except ValueError:
-            print ("Valor incorrecto")
+            print ("Opción inválida, vuelva a intentar")
 
+    if opt == 0:
+        print("\n¡Hasta luego!")
+        break
+    
+    if (opt == 1):
+        while True:
+            try:
+                nro_parcelas = int(input("Ingrese cantidad de parcelas: "))
+                recargo = int(input("Ingrese % de recargo de honorarios: "))
+                honorarios, honorarios_extra, sellado_cpam = replanteo_y_amojonamiento(nro_parcelas, recargo)
+                if recargo != 0:
+                    #print (type(honorarios_extra))
+                    print (f"\nHonorarios: ${honorarios_extra}")
+                    print (f"Sellado CPAM: ${sellado_cpam}")
+                    break
+                else:
+                    print (f"\nHonorarios: ${honorarios}")
+                    print (f"Sellado CPAM: ${sellado_cpam}")
+                    break
+            except ValueError:
+                print ("Valor incorrecto")
+
+        continuar = input("\n¿Desea realizar otro cálculo? (s/n): ").lower()
+        if continuar != 's':
+            print("Gracias por usar el programa.")
+            break
